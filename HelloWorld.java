002e207854ad0cd371d4dc9a0a787357240a13af
lavanya132 <lavanya132@gmail.com>
2018-01-11 22:25:34 -0600

    Adding two numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,9 @@ public class HelloWorld {
     
     public static void main(String[] args){
         System.out.println("Welcome to Java");
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.ptrintln("Addition of two numbers :"+c);      
     }
 }
